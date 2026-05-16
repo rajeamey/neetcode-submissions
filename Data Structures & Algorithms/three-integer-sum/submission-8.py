04099767f010c064dd5bb0ece8a02f6ffe7e3ac1
rajeamey <156326424+rajeamey@users.noreply.github.com>
2026-05-16 15:12:18 +0530

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        pairs = list()
+        nums = sorted(nums)
+        for i in range(len(nums)):
+
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                three_sum = nums[i] + nums[left] + nums[right]
+                if three_sum == 0:
+                    pairs.append([nums[i], nums[left], nums[right]])
+                    right -= 1
+                    left += 1
+                elif three_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
+        
+        unique_pairs = []
+        for pair in pairs:
+            if pair not in unique_pairs:
+                unique_pairs.append(pair)
+
+        return unique_pairs
